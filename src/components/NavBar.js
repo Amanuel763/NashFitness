99554b89414fef85ca45a6726c4d718d017cd155
Amanuel Afework <schwartzfuss763@yahoo.com>
2021-02-03 19:23:13 -0600

    Update NavBar.js

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -29,7 +29,7 @@ function Navbar() {
     <>
       <nav className='navbar'>
         <div className='navbar-container'>
-          <Link to='/' className='navbar-logo' onClick={closeMobileMenu}>
+          <Link to='/NashFitness/' className='navbar-logo' onClick={closeMobileMenu}>
             <GiWeightLiftingUp />
             NashFitness
           </Link>
